8024c6a916da40322ff9661aca2ea35a4ff0ec2c
gawdslayer <gawdslayer42069@gmail.com>
2025-04-02 11:22:24 +1100

    add new console log

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ const server = http.createServer((req, res) => {
   res.write('<h1>hello world</h1>');
   res.end();
 });
-
+console.log('Server running at http://localhost:3030/')
+// Start the server and listen on port 3030
 server.listen(3030)
